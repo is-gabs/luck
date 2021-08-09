68aae7a635f6c5f07cd040ffaae99c11339ac95c
Gabriel Sarmento <gabrielfs.bot@gmail.com>
2021-08-08 21:11:56 -0300

    impproving moviment methods

--- luck/player/hero.py
+++ luck/player/hero.py
@@ -1,26 +1,87 @@
+import pygame
+from pygame import key
 from pygame.image import load
+from pygame.sprite import Group
 from pygame.sprite import Sprite
+from pygame.transform import flip
 from pygame.transform import scale
 
-from luck.commons.constants import BLOCK_SIZE
+from luck.commons.constants import Directions
+from luck.commons.constants import FLOOR_Y
 from luck.commons.constants import GRAVITY
-from luck.commons.constants import HEIGHT
+from luck.commons.constants import PLAYER_HEIGHT
+from luck.commons.constants import SCREEN_WIDTH
 
 
 class Hero(Sprite):
-    def __init__(self, x: int = 0, y: int = 0) -> None:
+    def __init__(self, floor: Group, x: int = 0, y: int = 0) -> None:
         super().__init__()
 
-        self.image = scale(load("luck/player/hero.png"), (40, 40))
-
+        self.image = scale(
+            load("luck/player/hero.png"), (PLAYER_HEIGHT, PLAYER_HEIGHT)
+        )
         self.rect = self.image.get_rect()
+        self.floor = floor
+
+        self.height = PLAYER_HEIGHT
+        self.direction = Directions.RIGHT
 
         self.rect.x = x
         self.rect.y = y
 
-    def update(self) -> None:
-        self.fall()
+        self.speed_x = 9
+        self.speed_y = 0
 
-    def fall(self):
-        if self.rect.y < HEIGHT - BLOCK_SIZE - 40:
-            self.rect.y += GRAVITY
+        self.mass = 2
+
+        self.is_jumping = True
+
+    def update(self) -> None:
+        keys = key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.increase_x(self.speed_x * -1)
+            if self.direction == Directions.RIGHT:
+                self.direction = Directions.LEFT
+                self.image = flip(self.image, True, False)
+        if keys[pygame.K_RIGHT]:
+            self.increase_x(self.speed_x)
+            if self.direction == Directions.LEFT:
+                self.direction = Directions.RIGHT
+                self.image = flip(self.image, True, False)
+        if keys[pygame.K_UP]:
+            self.jump()
+
+        if self.is_jumping:
+            force = GRAVITY * 2 * self.mass * (self.speed_y * self.speed_y)
+
+            if self.speed_y < 0:
+                force = force * -1
+
+            if self.rect.y - force > FLOOR_Y:
+                self.rect.y = FLOOR_Y + 1
+            else:
+                self.rect.y -= force
+
+            self.speed_y -= 1
+
+            if self.is_on_floor:
+                self.speed_y = 6
+                self.is_jumping = False
+
+    def jump(self):
+        if self.is_on_floor:
+            self.is_jumping = True
+
+    def increase_x(self, speed):
+        self.rect.x += speed
+        if self.rect.x > SCREEN_WIDTH - 20:
+            self.rect.x = SCREEN_WIDTH - 20
+        elif self.rect.x < -10:
+            self.rect.x = -10
+
+    @property
+    def is_on_floor(self):
+        return any(
+            self.rect.colliderect(block) for block in self.floor.sprites()
+        )
--- run.py
+++ run.py
@@ -9,12 +9,15 @@ from pygame.time import Clock
 from pygame.transform import scale
 
 from luck.commons.constants import BLOCK_SIZE
-from luck.commons.constants import HEIGHT
+from luck.commons.constants import SCREEN_HEIGHT
 from luck.commons.constants import SCREEN_SIZE
+from luck.commons.constants import SCREEN_WIDTH
 from luck.commons.constants import TICK
-from luck.commons.constants import WIDTH
 from luck.player.hero import Hero
 from luck.structure.ground import MetalBlock
+
+RUNNING = True
+
 
 pygame.init()
 clock = Clock()
@@ -22,24 +25,28 @@ screen = display.set_mode(SCREEN_SIZE)
 background = scale(load("luck/commons/backgrounds/space.jpg"), SCREEN_SIZE)
 
 
-metal_block = MetalBlock(y=HEIGHT - BLOCK_SIZE)
-hero = Hero()
+metal_block = MetalBlock(y=SCREEN_HEIGHT - BLOCK_SIZE)
 
 ground_group = Group()
+
+hero = Hero(floor=ground_group, y=SCREEN_HEIGHT / 2)
+
 hero_group = GroupSingle(hero)
 
 
 def build_ground():
-    n_blocks = int(WIDTH / BLOCK_SIZE)
+    n_blocks = int(SCREEN_WIDTH / BLOCK_SIZE)
 
     for i in range(n_blocks):
-        ground_group.add(MetalBlock(x=i * BLOCK_SIZE, y=HEIGHT - BLOCK_SIZE))
+        ground_group.add(
+            MetalBlock(x=i * BLOCK_SIZE, y=SCREEN_HEIGHT - BLOCK_SIZE)
+        )
 
 
 build_ground()
 
 
-while True:
+while RUNNING:
     clock.tick(TICK)
 
     screen.blit(background, (0, 0))
@@ -50,8 +57,9 @@ while True:
     ground_group.update()
     hero_group.update()
 
-    display.update()
+    display.flip()
 
     for e in event.get():
         if e.type == locals.QUIT:
             pygame.quit()
+            RUNNING = False
